d9b0749f3efb9b3cb8da4eb39f1b1d48a91bab12
justfalter <falter@gmail.com>
2013-10-16 12:18:37 -0700

    Simplify event creation

--- lib/CIF/Models/Event.pm
+++ lib/CIF/Models/Event.pm
@@ -1,14 +1,50 @@
 package CIF::Models::Event;
-
+use strict;
+use warnings;
 use Scalar::Util qw(blessed);
 use Data::Dumper;
+#use Tie::Hash;
+#our @ISA = 'Tie::StdHash';
+
+use constant FIELDS => {
+  address => 1,
+  address_mask => 1,
+  alternativeid => 1,
+  alternativeid_restriction => 1,
+  assessment => 1,
+  carboncopy => 1,
+  confidence => 1,
+  contact => 1,
+  contact_email => 1,
+  description => 1,
+  detecttime => 1,
+  guid => 1,
+  hash => 1,
+  id => 1,
+  lang => 1,
+  malware_md5 => 1,
+  malware_sha1 => 1,
+  md5 => 1,
+  method => 1,
+  portlist => 1,
+  protocol => 1,
+  purpose => 1,
+  relatedid => 1,
+  reporttime => 1,
+  restriction => 1,
+  severity => 1,
+  sha1 => 1,
+  source => 1,
+  timezone => 1,
+  timestamp_epoch => 1
+};
 
 require JSON;
-
 sub new {
   my $class = shift;
   my $data = shift || {};
   my $self = {};
+  #tie %{$self}, $class;
   map { $self->{$_} = $data->{$_} } keys %{$data};
   bless $self, $class;
   return $self;
@@ -22,5 +58,6 @@ sub to_json {
   }
   return JSON::encode_json($data);
 }
+
 
 1;
