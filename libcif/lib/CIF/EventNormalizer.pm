6a3a2f72800d23273a5e0008680096528cb6b258
justfalter <falter@gmail.com>
2013-11-14 09:38:02 -0800

    Switched EventNormalizer to Moose

--- libcif/lib/CIF/EventNormalizer.pm
+++ libcif/lib/CIF/EventNormalizer.pm
@@ -3,47 +3,58 @@ package CIF::EventNormalizer;
 use strict;
 use warnings;
 use CIF qw/normalize_timestamp debug/;
+use Moose;
+use namespace::autoclean;
 use Module::Pluggable search_path => "CIF::EventNormalizers", 
-      require => 1, sub_name => '_preprocessors';
+      require => 1, sub_name => '__preprocessors';
 
-sub new {
-  my $class = shift;
-  my $args = shift;
-  
-  my $self = {
-    refresh => $args->{refresh} || 0,
-    severity_map => $args->{severity_map},
-    goback => $args->{goback},
-    now => DateTime->from_epoch(epoch => time())
-  };
-  bless $self, $class;
 
-  $self->{preprocessors} = $self->_init_preprocessors();
+has 'refresh' => (
+  is => 'bare',
+  reader => '_refresh',
+  default => 0,
+  required => 1
+);
 
-  return $self;
-}
+has 'severity_map' => (
+  is => 'bare',
+  reader => '_severity_map',
+  required => 1
+);
 
-sub _init_preprocessors {
-  my $self = shift;
-  my @ret = ();
-  foreach my $preprocessor (__PACKAGE__->_preprocessors()) {
-    push(@ret, $preprocessor);
-  }
-  return \@ret;
-}
+has 'goback' => (
+  is => 'bare', 
+  isa => 'Int',
+  reader => '_goback',
+  required => 1
+);
+
+has '_now' => (
+  is => 'ro', 
+  default => sub { DateTime->from_epoch(epoch => time()) },
+  init_arg => undef
+);
+
+has '_preprocessors' => (
+  traits => ['Array'],
+  is => 'ro', 
+  isa => 'ArrayRef[Str]',
+  default => sub { [__preprocessors()]; },
+  init_arg => undef
+);
 
 sub normalize {
   my $self = shift;
   my $r = shift;
 
-  my $now  = $self->{now};
+  my $now  = $self->_now;
   my $dt = $r->{'detecttime'} || $now;
   my $rt = $r->{'reporttime'} || $now;
 
   $dt = normalize_timestamp($dt,$now);
   my $timestamp_epoch;
 
-  if($self->{refresh}){
+  if($self->_refresh){
     $rt = $now;
     $timestamp_epoch = $now->epoch();
   } else {
@@ -54,7 +65,7 @@ sub normalize {
   $r->{'detecttime'}        = $dt->epoch();
   $r->{'reporttime'}        = $rt->epoch();
 
-  if($timestamp_epoch < $self->{goback}) { 
+  if($timestamp_epoch < $self->_goback) { 
     return(undef);
   }
 
@@ -76,15 +87,22 @@ sub normalize {
     $r->{'assessment'} = 'unknown';
   }
 
-  foreach my $p (@{$self->{preprocessors}}){
+  foreach my $p (@{$self->_preprocessors}){
     $r = $p->process($r);
   }
 
   # TODO -- work-around, make this more configurable
   unless($r->{'severity'}){
-    $r->{'severity'} = (defined($self->{severity_map}->{$r->{'assessment'}})) ? $self->{severity_map}->{$r->{'assessment'}} : 'medium';
+    my $sm = $self->_severity_map;
+    if (my $severity = $self->_severity_map->{$r->{'assessment'}}) {
+      $r->{'severity'} = $severity;
+    } else {
+      $r->{'severity'} = 'medium'; 
+    }
   }
   return $r;
 }
+
+__PACKAGE__->meta->make_immutable;
 
 1;
