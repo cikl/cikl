8c993d646044099b6b10d28ee9417fa14d9fdae3
justfalter <falter@gmail.com>
2013-11-19 12:59:30 -0800

    ParseRss now able to take well-formed arguments.

---
+++ cif-smrt/lib/CIF/Smrt/ParserHelpers/RegexMapping.pm
@@ -0,0 +1,50 @@
+package CIF::Smrt::ParserHelpers::RegexMapping;
+
+use strict;
+use warnings;
+use Moose;
+use Moose::Util::TypeConstraints;
+
+
+has 'name' => (
+  is => 'ro',
+  isa => 'Str',
+  required => 1
+);
+
+has 'regex' => (
+  is => 'ro',
+  isa => 'RegexpRef',
+  required => 1
+);
+
+has 'event_fields' => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  required => 1
+);
+
+sub parse {
+  my $self = shift;
+  my $data = shift;
+  my $ret = {};
+  return undef unless(defined($data));
+  my @matches = ($data =~ $self->regex);
+  return undef if ($#matches == -1);
+  my $i = 0;
+  foreach my $field_name (@{$self->event_fields}) {
+    $ret->{$field_name} = $matches[$i];
+    $i++;
+  }
+
+  return $ret;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+subtype 'RegexMappingsRequired',
+  as 'ArrayRef[CIF::Smrt::ParserHelpers::RegexMapping]',
+  where { $#{$_[0]} > -1 },
+  message { "'ERROR: at least one RegexMapping is required'" };
+
+1;
--- cif-smrt/lib/CIF/Smrt/Parsers/ParseRss.pm
+++ cif-smrt/lib/CIF/Smrt/Parsers/ParseRss.pm
@@ -2,23 +2,67 @@ package CIF::Smrt::Parsers::ParseRss;
 
 use strict;
 use warnings;
-
+use CIF::Smrt::ParserHelpers::RegexMapping;
 use Moose;
 use CIF::Smrt::Parser;
-extends 'CIF::Smrt::Parser';
 use namespace::autoclean;
-
 use XML::RSS::LibXML;
+
+extends 'CIF::Smrt::Parser';
+
 
 use constant NAME => 'rss';
 sub name { return NAME; }
+
+around BUILDARGS => sub {
+  my $orig_method = shift;
+  my $class = shift;
+  my %args = @_;
+  use Data::Dumper;
+
+  # Don't do anything if it rss_regex_map already exists.
+  if (exists($args{rss_regex_map})) {
+    return $class->$orig_method(%args);
+  }
+
+  my @regex_map;
+  foreach my $key (keys %args) {
+    if ($key =~ /^regex_(.*)_values$/) {
+      my $name = $1;
+      my $regex = $args{"regex_${name}"};
+      my $values = $args{"regex_${name}_values"};
+      if (!defined($regex)) {
+        die("Missing matching 'regex_{$name}' for 'regex_${name}_values'!");
+      }
+      my @split_values = split(/\s*,\s*/, $values);
+      my $m = CIF::Smrt::ParserHelpers::RegexMapping->new(
+        name => $name,
+        regex => qr/$regex/,
+        event_fields => \@split_values
+      );
+      push(@regex_map, $m);
+    }
+  }
+  $args{rss_regex_map} = \@regex_map;
+
+  return $class->$orig_method(%args);
+};
+
+has 'rss_regex_map' => (
+  traits => ['Array'],
+  is => 'ro',
+  isa => 'RegexMappingsRequired',
+  required => 1
+);
 
 sub parse {
     my $self = shift;
     my $content_ref = shift;
     my $broker = shift;
     my $content = $$content_ref;
-    
+
+    my $regex_map = $self->rss_regex_map;
+
     # fix malformed RSS
     unless($content =~ /^<\?xml version/){
         $content = '<?xml version="1.0"?>'."\n".$content;
@@ -36,29 +80,27 @@ sub parse {
     $content = join("\n",@lines);
     $rss->parse($content);
     foreach my $item (@{$rss->{items}}){
+        # Conditions for bailing:
+        #   - Any of the keys missing (incomplete record).
+        #   - Any of the regexes not matching.
+        
         my $h = {};
-        my $found = 1;
-        foreach my $key (keys %$item){
-            if (my $r = $self->config->keyed_regex($key)){
-                my @m = ($item->{$key} =~ /$r/);
-                if ($#m == -1) {
-                  $found = 0;
-                  last
-                }
-                my @cols = $self->config->keyed_regex_values($key);
-                foreach (0 ... $#cols){
-                    $h->{$cols[$_]} = $m[$_];
-                }
-            }
+        foreach my $mapping (@$regex_map) {
+          if (my $ret = $mapping->parse($item->{$mapping->name})) {
+            # Merge the data;
+            $h = {%$h, %$ret};
+          } else {
+            goto(SKIPIT);
+          }
         }
-        if ($found == 1) {
-          $broker->emit($h);
-        }
+        $broker->emit($h);
+SKIPIT:
     }
     return(undef);
 
 }
 
 __PACKAGE__->meta->make_immutable;
+
 
 1;
