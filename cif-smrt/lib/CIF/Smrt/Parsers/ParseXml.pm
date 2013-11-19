c274f6021cec8badbbbf831fdcfb56b0640731e6
justfalter <falter@gmail.com>
2013-11-19 13:06:21 -0800

    Remove unused code

--- cif-smrt/lib/CIF/Smrt/Parsers/ParseXml.pm
+++ cif-smrt/lib/CIF/Smrt/Parsers/ParseXml.pm
@@ -49,24 +49,6 @@ sub parse {
     my @attributes_map  = $self->config->attributes_map; 
     my @attributes      = $self->config->attributes; 
     
-    my %regex;
-    # TODO MPR: clean this up. Modifying the config is bonkers.
-    foreach my $k (keys %{$self->config}){
-        # pull out any custom regex
-        for($k){
-            if(/^regex_(\S+)$/){
-                $regex{$1} = qr/$self->config->{$k}/;
-                #delete($self->config->{$k});
-                last;
-            }
-            # clean up the hash, so we can re-map the default values later
-            if(/^(elements_?|attributes_?|node|subnode)/){
-              #delete($self->config->{$k});
-                last;
-            }
-        }
-    }
-   
     foreach my $node (@nodes){
         my $h = {};
         my $found = 0;
@@ -74,7 +56,6 @@ sub parse {
             foreach my $e (0 ... $#elements_map){
                 my $x = $node->findvalue('./'.$elements[$e]);
                 next unless($x);
-                #if(my $r = $regex{$elements[$e]}){
                 if(my $r = $self->config->regex_for($elements[$e])){
                     if($x =~ $r){
                         $h->{$elements_map[$e]} = $x;
@@ -91,7 +72,6 @@ sub parse {
             foreach my $e (0 ... $#attributes_map){       
                 my $x = $node->getAttribute($attributes[$e]);
                 next unless($x);
-                #if(my $r = $regex{$attributes[$e]}){
                 if(my $r = $self->config->regex_for($attributes[$e])){
                     if($x =~ $r){
                         $h->{$attributes_map[$e]} = $x;
