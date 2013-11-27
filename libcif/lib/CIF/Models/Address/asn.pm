fec567441bd8f5f5587a294877fb420502accd21
justfalter <falter@gmail.com>
2013-11-27 11:37:14 -0800

    Validation for ASN

--- libcif/lib/CIF/Models/Address/asn.pm
+++ libcif/lib/CIF/Models/Address/asn.pm
@@ -3,10 +3,15 @@ use strict;
 use warnings;
 use Moose;
 use CIF::Models::AddressRole;
+use CIF::MooseTypes;
 use namespace::autoclean;
 with 'CIF::Models::AddressRole';
 
 sub type { 'asn' }
+
+has '+value' => (
+  isa => 'CIF::MooseTypes::Asn',
+);
 
 __PACKAGE__->meta->make_immutable;
 1;
---
+++ libcif/lib/CIF/MooseTypes/Asn.pm
@@ -0,0 +1,15 @@
+package CIF::MooseTypes::Asn;
+use strict;
+use warnings;
+use namespace::autoclean;
+use Moose::Util::TypeConstraints;
+
+use constant MAX_ASN => 2**32 - 1;
+
+subtype 'CIF::MooseTypes::Asn',
+  as 'Int',
+  where { $_ > 0 && $_ <= MAX_ASN },
+  message { "Invalid ASN: $_"} ;
+1;
+
+
