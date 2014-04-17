6a0330fd77ad93b5e02372b53ed95339d79351a2
Mike Ryan <falter@gmail.com>
2014-04-17 13:07:40 -0500

    Enable installation of Cikl

--- puppet/private_modules/cikl/manifests/smrt/install.pp
+++ puppet/private_modules/cikl/manifests/smrt/install.pp
@@ -1,18 +1,15 @@
 class cikl::smrt::install () {
   include cikl::smrt::deps
 
-#  perl::module { 'Cikl': 
-#    require     => Class['cikl::smrt::deps'],
-#    exec_environment => [ 'PERL_CPANM_OPT=--notest --skip-satisfied' ]
-#  } ->
-#  perl::module { 'Cikl::RabbitMQ': 
-#    exec_environment => [ 'PERL_CPANM_OPT=--notest --skip-satisfied' ]
-#  }
-  
-#  exec { 'install Cikl': 
-#    command => '/usr/bin/cpanm --notest --skip-satisfied Cikl Cikl::RabbitMQ',
-#    require => Class['cikl::smrt::deps']
-#  }
+  perl::module { 'Cikl': 
+    require          => Class['cikl::smrt::deps'],
+    exec_environment => [ 'PERL_CPANM_OPT=--notest --skip-satisfied' ],
+    timeout          => 0
+  } ->
+  perl::module { 'Cikl::RabbitMQ': 
+    exec_environment => [ 'PERL_CPANM_OPT=--notest --skip-satisfied' ],
+    timeout          => 0
+  }
 
   # Generate cikl.conf
   file { 'cikl-conf': 
