306f309a86530ecef1f3c8d419c6f71245eba433
Mike Ryan <falter@gmail.com>
2014-04-18 13:22:03 -0500

    Fix variable references

--- puppet/private_modules/cikl/manifests/kibana/install.pp
+++ puppet/private_modules/cikl/manifests/kibana/install.pp
@@ -1,7 +1,9 @@
 class cikl::kibana::install {
+  $kibana_base = $cikl::kibana_base
+  $kibana_root = $cikl::kibana_root
   $kibana_version = '3.0.1'
   $kibana_name = "kibana-${kibana_version}"
-  $kibana_install_path = "${cikl::kibana_base}/${kibana_name}"
+  $kibana_install_path = "${kibana_base}/${kibana_name}"
   $kibana_tgz = "${kibana_name}.tar.gz"
   $kibana_conf = "${kibana_root}/config.js"
 
@@ -14,7 +16,7 @@ class cikl::kibana::install {
   }
 
   staging::extract { $kibana_tgz:
-    target  => $cikl::kibana_base,
+    target  => $kibana_base,
     creates => $kibana_install_path,
     require => [
       Staging::File[$kibana_tgz],
