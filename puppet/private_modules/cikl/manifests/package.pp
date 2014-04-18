d1d418af17b17e6160617fe086196e918fae7512
Mike Ryan <falter@gmail.com>
2014-04-18 13:23:21 -0500

    Simplify package setup

--- puppet/private_modules/cikl/manifests/package.pp
+++ puppet/private_modules/cikl/manifests/package.pp
@@ -1,7 +1,18 @@
 define cikl::package (
-  $package_name, 
+  $debian = undef,
+  $redhat = undef,
   $ensure = 'present'
 ) {
+
+  $package_name = $::osfamily ? {
+    'Debian' => $debian,
+    'RedHat' => $redhat,
+    default  => undef
+  }
+
+  if ($package_name == undef) {
+    fail("\"${module_name}\" provides no repository information for OSfamily \"${::osfamily}\"")
+  }
 
   if (!defined(Package[$name])) {
     package { $name:
--- puppet/private_modules/cikl/manifests/packages/build-essential.pp
+++ puppet/private_modules/cikl/manifests/packages/build-essential.pp
@@ -1,20 +1,13 @@
 class cikl::packages::build-essential () {
-  case $::osfamily {
-    'Debian' : {
-      cikl::package { 'cikl::packages::build-essential':
-        package_name => 'build-essential'
-      }
+  if ($::osfamily == 'RedHat') {
+    exec { 'install redhat development tools':
+      command => '/usr/bin/yum -d 0 -e 0 -y groupinstall "Development Tools"',
+      unless  => '/usr/bin/yum -C grouplist "Development Tools"| /bin/grep "^Installed Groups"'
     }
+  } else {
 
-    'RedHat' : {
-      exec { 'install redhat development tools':
-        command => '/usr/bin/yum -d 0 -e 0 -y groupinstall "Development Tools"',
-        unless  => '/usr/bin/yum -C grouplist "Development Tools"| /bin/grep "^Installed Groups"'
-      }
-    }
-
-    default: {
-      fail("\"${module_name}\" provides no repository information for OSfamily \"${::osfamily}\"")
+    cikl::package { 'cikl::packages::build-essential':
+      debian => 'build-essential',
     }
   }
 }
