a19abd0fe4d051021a15a67376d88d0925f0fd00
Mike Ryan <falter@gmail.com>
2014-05-02 19:06:02 -0500

    Remove unused file

--- puppet/private_modules/cikl/manifests/repo/yum.pp
+++
@@ -1,6 +0,0 @@
-class cikl::repo::yum {
-  # Enable EPEL repositories
-  anchor { 'cikl::repo::yum::begin': } ->
-  anchor { 'cikl::repo::yum::end': }
-  class { '::epel': } -> Anchor['cikl::repo::yum::end']
-}
