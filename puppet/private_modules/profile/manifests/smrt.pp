bc8e194ea4891e36ee47109d8c092d343ef61a1f
Mike Ryan <falter@gmail.com>
2014-05-06 16:57:57 -0500

    Make smrt usable for dev

--- puppet/private_modules/profile/manifests/smrt.pp
+++ puppet/private_modules/profile/manifests/smrt.pp
@@ -3,13 +3,50 @@ class profile::smrt (
   $rabbitmq_port = 5672,
   $rabbitmq_username = 'guest',
   $rabbitmq_password = 'guest',
-  $rabbitmq_vhost = '/'
+  $rabbitmq_vhost = '/',
+  $local_p5_cikl            = undef,
+  $local_p5_cikl_rabbitmq   = undef,
 ) inherits profile::base {
 
   $smrt_perllib_dir  = '/opt/cikl/smrt'
 
-  $perl5lib = "/vagrant/p5-Cikl/lib:/vagrant/p5-Cikl-RabbitMQ/lib:$smrt_perllib_dir/lib/perl5"
-  $append_to_path = "/vagrant/p5-Cikl/bin"
+  if ($local_p5_cikl == undef) {
+    $p5_cikl_libpath = []
+    $p5_cikl_binpath = []
+    $p5_cikl_install_args = "Cikl"
+  } else {
+    $p5_cikl_libpath = ["$local_p5_cikl/lib"]
+    $p5_cikl_binpath = ["$local_p5_cikl/bin"]
+    validate_absolute_path($local_p5_cikl)
+    $p5_cikl_install_args = "--installdeps $local_p5_cikl"
+  }
+
+  if ($local_p5_cikl_rabbitmq == undef) {
+    $p5_cikl_rabbitmq_libpath = []
+    $p5_cikl_rabbitmq_binpath = []
+    $p5_cikl_rabbitmq_install_args = "Cikl::RabbitMQ"
+  } else {
+    $p5_cikl_rabbitmq_libpath = ["$local_p5_cikl_rabbitmq/lib"]
+    $p5_cikl_rabbitmq_binpath = ["$local_p5_cikl_rabbitmq/bin"]
+    validate_absolute_path($local_p5_cikl)
+    $p5_cikl_rabbitmq_install_args = "--installdeps $local_p5_cikl_rabbitmq"
+  }
+
+  $perl5lib = join(flatten(
+    [
+      $p5_cikl_libpath,
+      $p5_cikl_rabbitmq_libpath,
+      ["$smrt_perllib_dir/lib/perl5"]
+    ]
+  ), ":")
+
+  $append_to_path = join(flatten(
+    [
+      $p5_cikl_binpath,
+      $p5_cikl_rabbitmq_binpath,
+      ["$smrt_perllib_dir/bin"]
+    ]
+  ), ":")
 
   class profile::smrt::deps {
     ensure_packages([
@@ -34,11 +71,17 @@ class profile::smrt (
     owner   => "root",
     group   => "root",
     mode    => '0644',
+    # Template uses the following variables:
+    #  $rabbitmq_host
+    #  $rabbitmq_port
+    #  $rabbitmq_username
+    #  $rabbitmq_password
+    #  $rabbitmq_vhost
     content => template('profile/smrt/cikl.conf.erb')
   }
 
   exec { 'profile::smrt::install':
-    command     => "/usr/bin/cpanm --notest -l $smrt_perllib_dir --installdeps /vagrant/p5-Cikl",
+    command     => "/usr/bin/cpanm --notest -l $smrt_perllib_dir --no-man-pages --save-dists $smrt_perllib_dir/cache $p5_cikl_install_args",
     environment => "PERL5LIB=$perl5lib",
     require => [ 
       Class['profile::smrt::deps'],
@@ -47,7 +90,7 @@ class profile::smrt (
   }
 
   exec { 'profile::smrt::install_rabbitmq':
-    command => "/usr/bin/cpanm --notest -l $smrt_perllib_dir --installdeps /vagrant/p5-Cikl-RabbitMQ",
+    command => "/usr/bin/cpanm --notest -l $smrt_perllib_dir --no-man-pages --save-dists $smrt_perllib_dir/cache $p5_cikl_rabbitmq_install_args",
     environment => "PERL5LIB=$perl5lib",
     require => [ 
       Class['profile::smrt::deps'],
@@ -57,6 +100,9 @@ class profile::smrt (
   }
 
   file { "/etc/profile.d/cikl_profile.sh":
+    # Templates uses the following variables:
+    #  $append_to_path
+    #  $perl5lib
     content => template('profile/smrt/profile.sh.erb')
   }
 }
