defb02fabe745791babc629c5a0355d7ebe49f17
justfalter <falter@gmail.com>
2014-05-13 17:28:19 -0500

    (cikl/cikl#57) Run apt-get update before packages

--- puppet/private_modules/profile/manifests/init.pp
+++ puppet/private_modules/profile/manifests/init.pp
@@ -1,2 +1,12 @@
 class profile::base {
+  if !defined(Class['apt']) {
+    class { 'apt': }
+  }
+
+  exec { 'cikl::apt-update': 
+    command => '/usr/bin/apt-get update -y -q'
+  }
+
+  # Ensure that apt-get update is run prior to installing any packages via apt
+  Exec['cikl::apt-update'] -> Package <| provider == 'apt' |>
 }
