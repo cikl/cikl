5f2927df638d87048316403ae9e595c012da0a6f
justfalter <falter@gmail.com>
2014-07-08 15:20:20 -0500

    Improve ipv4 unit tests

--- cikl-api/spec/support/shared/ipv4.rb
+++ cikl-api/spec/support/shared/ipv4.rb
@@ -1,87 +1,58 @@
 RSpec.shared_examples_for 'an IPv4 query endpoint' do
+  include APIHelpers
+
+  shared_examples_for 'ipv4 query endpoint examples' do |ipv4, observable_sets|
+    it_should_behave_like 'a proper API endpoint when matching 1 or more events'
+
+    describe "the result" do
+      let(:result) { MultiJson.load(last_response.body) }
+      subject { result }
+      its(["count"]) { is_expected.to eq(observable_sets.count) }
+      its(["total_events"]) { is_expected.to eq(observable_sets.count) }
+      describe 'the events' do
+        let(:events) { result['events'] }
+        subject { events }
+        its(:count) { is_expected.to eq(observable_sets.count) }
+        observable_sets.each do |type, field|
+          it "should include an event where #{type}.#{field} equals '#{ipv4}'" do
+            expect(events).to include ( 
+              an_event_with_observable(type, field => ipv4)
+            )
+          end
+        end
+      end
+    end
+  end
+
   it_should_behave_like 'a proper API endpoint when matching 0 events' do
     before :each do
       query_proc.call('255.100.1.1')
     end
   end
 
-  context "matching against ipv4.ipv4" do
+  context "matching against ipv4.ipv4 with a query for '100.1.1.1'" do
+    include_examples 'ipv4 query endpoint examples', '100.1.1.1', [['ipv4', 'ipv4']]
     before :each do
       query_proc.call('100.1.1.1')
     end
-
-    it_should_behave_like 'a proper API endpoint when matching 1 or more events'
-
-    describe 'the response data' do
-      let(:result) { MultiJson.load(last_response.body) }
-      it "should only have the matching events" do
-        expect(result['total_events']).to eq(1)
-        expect(result['count']).to eq(1)
-        expect(result['events'].length).to eq(1)
-        event = result['events'].first
-
-        expect(event['observables']).to eq(
-          {
-            'ipv4' => [
-              {
-                'ipv4' => '100.1.1.1'
-              }
-            ]
-          }
-        )
-      end
-    end
-
   end
 
-  context "matching against dns_answer.ipv4" do
+  context "matching against dns_answer.ipv4 with a query for '100.1.3.1'" do
+    include_examples 'ipv4 query endpoint examples', '100.1.3.1', [['dns_answer', 'ipv4']]
     before :each do
       query_proc.call('100.1.3.1')
     end
-
-    it_should_behave_like 'a proper API endpoint when matching 1 or more events'
-
-    describe 'the response data' do
-      let(:result) { MultiJson.load(last_response.body) }
-      it "should only have the matching events" do
-        expect(result['total_events']).to eq(1)
-        expect(result['count']).to eq(1)
-        expect(result['events'].length).to eq(1)
-        event = result['events'].first
-        expect(event['observables'].count).to eq(1)
-        dns_answer = event['observables']['dns_answer'][0]
-        expect(dns_answer['ipv4']).to eq('100.1.3.1')
-      end
-    end
   end
 
-  context "matching against ipv4.ipv4 and dns_answer.ipv4 at the same time" do
+  context "matching against ipv4.ipv4 and dns_answer.ipv4 with a query for '100.1.2.1'" do
     before :each do
       query_proc.call('100.1.2.1')
     end
 
-    it_should_behave_like 'a proper API endpoint when matching 1 or more events'
+    include_examples 'ipv4 query endpoint examples', '100.1.2.1', [
+      ['dns_answer', 'ipv4'],
+      ['ipv4', 'ipv4']
+    ]
 
-    describe 'the response data' do
-      let(:result) { MultiJson.load(last_response.body) }
-      it "should only have the matching events" do
-        expect(result['total_events']).to eq(2)
-        expect(result['count']).to eq(2)
-        expect(result['events'].length).to eq(2)
-        event1 = result['events'][0]
-        event2 = result['events'][1]
-        dns_answer = ipv4 = nil
-
-        if event1['observables']['dns_answer']
-          dns_answer = event1['observables']['dns_answer'][0]
-          ipv4 = event2['observables']['ipv4'][0]
-        else 
-          dns_answer = event2['observables']['dns_answer'][0]
-          ipv4 = event1['observables']['ipv4'][0]
-        end
-        expect(dns_answer['ipv4']).to eq('100.1.2.1')
-        expect(ipv4['ipv4']).to eq('100.1.2.1')
-      end
-    end
   end
 end
