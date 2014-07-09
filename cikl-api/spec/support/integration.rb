b29b7dbd8baedfeef610363077fa46bc59f7fd50
justfalter <falter@gmail.com>
2014-07-09 11:46:35 -0500

    Speed up fixture loading

--- cikl-api/spec/support/integration.rb
+++ cikl-api/spec/support/integration.rb
@@ -7,25 +7,35 @@ RSpec.shared_context "integration tests", :integration do
       config.add_setting :elasticsearch_port, default: 9250
 
       config.before(:suite) do
+        start = Time.now
+        ENV['TEST_CLUSTER_NODES'] = '1'
         require 'elasticsearch/extensions/test/cluster'
-
-        Elasticsearch::Extensions::Test::Cluster.start(
-          nodes: 1,
-          name: RSpec.configuration.elasticsearch_cluster_name,
-          port: RSpec.configuration.elasticsearch_port,
-          es_params: '-D es.network.host=localhost'
-        )
-
-        at_exit do
-          Elasticsearch::Extensions::Test::Cluster.stop(
-            name: RSpec.configuration.elasticsearch_cluster_name,
-            port: RSpec.configuration.elasticsearch_port
+        already_running = 
+          Elasticsearch::Extensions::Test::Cluster.running?(
+            on: RSpec.configuration.elasticsearch_port,
+            as: RSpec.configuration.elasticsearch_cluster_name,
           )
+
+        unless already_running == true
+          Elasticsearch::Extensions::Test::Cluster.start(
+            nodes: 1,
+            name: RSpec.configuration.elasticsearch_cluster_name,
+            port: RSpec.configuration.elasticsearch_port,
+            es_params: '-D es.network.host=localhost'
+          )
+
+          at_exit do
+            Elasticsearch::Extensions::Test::Cluster.stop(
+              name: RSpec.configuration.elasticsearch_cluster_name,
+              port: RSpec.configuration.elasticsearch_port
+            )
+          end
         end
 
         # Unnescessary?
         Fixtures::Loader.destroy!
-        Fixtures::Loader.load!
+        loader = Fixtures::Loader.new
+        loader.load!
       end
 
       config.after(:suite) do
