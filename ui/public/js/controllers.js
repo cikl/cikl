4d6e1fcd45dc6fa06173be03265336ce8e196d28
ds <dsweet04@gmail.com>
2014-07-11 13:41:33 -0700

    update to the ui (partial)

--- ui/public/js/controllers.js
+++ ui/public/js/controllers.js
@@ -11,6 +11,88 @@ var app = angular.module('ciklApp', [
 app.controller("SearchCtrl", function($scope, $http) {
   $scope.term = {};
 
+  $scope.import_min = new Date();
+  $scope.import_max = new Date();
+  $scope.detect_min = new Date();
+  $scope.detect_max = new Date();
+
+  $scope.detect_min.setDate($scope.detect_min.getDate() -30);
+  $scope.import_min.setDate($scope.import_min.getDate() -30);
+
+
+
+
+  DetectMinTimeCtrl = function ($scope) {
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+  };
+
+  DetectMinDateCtrl = function ($scope) {
+    $scope.today = function() {
+      $scope.detect_min = $;
+    };
+
+    $scope.clear = function () {
+      $scope.detect_min = null;
+    };
+
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+
+    $scope.format = 'MMMM dd, yyyy';
+  };
+
+  DetectMaxTimeCtrl = function ($scope) {
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+  };
+
+  DetectMaxDateCtrl = function ($scope) {
+    $scope.today = function() {
+      $scope.detect_max = new Date();
+    };
+
+    $scope.clear = function () {
+      $scope.detect_max = null;
+    };
+
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+
+    $scope.format = 'MMMM dd, yyyy';
+  };
+
+
+
+
+
+
   // ngShow boolean
   $scope.searched = false;
 
@@ -24,26 +106,13 @@ app.controller("SearchCtrl", function($scope, $http) {
   $scope.currentPage = 1;
 
   // Search page top form requests
-  $scope.update = function(search) {
-    $scope.term = search.term;
-    $scope.type = search.type;
+  $scope.search = function(artifact) {
+    $scope.term = artifact.term;
+    $scope.type = artifact.type;
+    $scope.searched = true;
 
-
-    if ($scope.type === 'ip-address') {
-      $http.post('http://localhost:8080/api/v1/query/ipv4.json', {ipv4:$scope.term}).
-          success(function (data) {
-            $scope.query = data;
-          }).
-          then(function() {
-            $scope.searched = true;
-
-            // Pagination Settings
-            $scope.totalItems = parseInt($scope.query.total_events);
-          });
-    }
-    else if ($scope.type === 'dns') {
-      $scope.current();
-    }
+    // Search Function
+    $scope.update();
   };
 
   // Sort button functions
@@ -95,39 +164,63 @@ app.controller("SearchCtrl", function($scope, $http) {
   $scope.isCollapsedDetect = true;
 
   $scope.$watch ('currentPage', function () {
-    $scope.current();
+    $scope.update();
   });
 
+  // Sort watch functions
   $scope.$watch ('orderBy', function () {
-    $scope.current();
+    $scope.update();
   });
 
   $scope.$watch ('order', function () {
-    $scope.current();
+    $scope.update();
   });
 
 
   // API requests Function
-  $scope.current = function () {
-    $http.post('http://localhost:8080/api/v1/query/fqdn.json',
-        {
-          start: 1 + ( ($scope.currentPage-1) * 10),
-          per_page: $scope.itemsPerPage,
-          order_by: $scope.orderBy,
-          order: $scope.order,
-          timing: 1,
-          fqdn: $scope.term
-        }).
-        success(function (data) {
-          $scope.query = data;
-        }).
-        then(function() {
-          $scope.searched = true;
+  $scope.update = function () {
 
-          // Pagination Settings
-          $scope.totalItems = parseInt($scope.query.total_events);
-        }
-    );
+    if ($scope.type === 'ip-address') {
+      $http.post('http://localhost:8080/api/v1/query.json',
+          {
+            start: 1 + ( ($scope.currentPage-1) * 10),
+            per_page: $scope.itemsPerPage,
+            order_by: $scope.orderBy,
+            order: $scope.order,
+            timing: 1,
+            ipv4: $scope.term
+          }).
+          success(function (data) {
+            $scope.query = data;
+
+            // For pagination
+            $scope.totalItems = parseInt($scope.query.total_events);
+          }).
+          then(function() {
+
+          });
+    }
+    else if ($scope.type === 'dns') {
+      $http.post('http://localhost:8080/api/v1/query.json',
+          {
+            start: 1 + ( ($scope.currentPage-1) * 10),
+            per_page: $scope.itemsPerPage,
+            order_by: $scope.orderBy,
+            order: $scope.order,
+            timing: 1,
+            fqdn: $scope.term
+          }).
+          success(function (data) {
+            $scope.query = data;
+
+            // For pagination
+            $scope.totalItems = parseInt($scope.query.total_events);
+          }).
+          then(function() {
+
+          }
+      );
+    }
   };
 
 
@@ -138,7 +231,7 @@ app.controller("SearchCtrl", function($scope, $http) {
 var DatepickerMinCtrl = function ($scope) {
   $scope.today = function() {
     $scope.date = new Date();
-    $scope.date.setDate($scope.date.getDate() - 1);
+    $scope.date.setDate($scope.date.getDate() -30);
   };
   $scope.today();
 
@@ -160,6 +253,7 @@ var DatepickerMinCtrl = function ($scope) {
 
   $scope.format = 'MMMM dd, yyyy';
 };
+
 
 var TimepickerMinCtrl = function ($scope) {
   $scope.date = new Date();
@@ -197,6 +291,7 @@ var DatepickerMaxCtrl = function ($scope) {
 
   $scope.format = 'MMMM dd, yyyy';
 };
+
 
 var TimepickerMaxCtrl = function ($scope) {
   $scope.date = new Date();
