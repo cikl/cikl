728b3e7e7d5935e96961cb482f1e257eb6bbb1fd
ds <dsweet04@gmail.com>
2014-07-11 18:21:36 -0700

    Added time filter functions.

--- ui/public/js/controllers.js
+++ ui/public/js/controllers.js
@@ -8,18 +8,48 @@ var app = angular.module('ciklApp', [
   'template/syTimepicker/popup.html'
 ]);
 
-app.controller("SearchCtrl", function($scope, $http) {
+app.controller("MainCtrl", function($scope, $http) {
+  // Search Variables
   $scope.term = {};
+  $scope.type = {};
 
+  // ngShow boolean
+  $scope.searched = false;
+
+  // Sort initial type
+  $scope.orderBy = 'import_time';
+  $scope.order = 'desc';
+
+  // Pagination settings variables
+  $scope.itemsPerPage = 20;
+  $scope.maxSize = 10;
+  $scope.currentPage = 1;
+
+  // Filter button collapse variables
+  $scope.isCollapsedImport = true;
+  $scope.isCollapsedDetect = true;
+
+  // Date and Time Variables
   $scope.import_min = new Date();
   $scope.import_max = new Date();
   $scope.detect_min = new Date();
   $scope.detect_max = new Date();
 
-  $scope.detect_min.setDate($scope.detect_min.getDate() -30);
-  $scope.import_min.setDate($scope.import_min.getDate() -30);
+  $scope.detect_min.setUTCDate($scope.detect_min.getUTCDate() -30);
+  $scope.import_min.setUTCDate($scope.import_min.getUTCDate() -30);
 
+  // Date and Time parent variables for child scopes
+  var m = this;
 
+  m.filter_import_min = new Date();
+  m.filter_import_max = new Date();
+  m.filter_detect_min = new Date();
+  m.filter_detect_max = new Date();
+
+  m.filter_detect_min.setUTCDate(m.filter_detect_min.getUTCDate() -30);
+  m.filter_import_min.setUTCDate(m.filter_import_min.getUTCDate() -30);
+
+  // Detect Min Time
   DetectMinTimeCtrl = function ($scope) {
     $scope.open = function($event) {
       $event.preventDefault();
@@ -29,9 +59,10 @@ app.controller("SearchCtrl", function($scope, $http) {
     };
   };
 
+  // Detect Min Date
   DetectMinDateCtrl = function ($scope) {
     $scope.today = function() {
-      $scope.detect_min = $;
+      $scope.detect_min = new Date();
     };
 
     $scope.clear = function () {
@@ -53,6 +84,7 @@ app.controller("SearchCtrl", function($scope, $http) {
     $scope.format = 'MMMM dd, yyyy';
   };
 
+  // Detect Max Time
   DetectMaxTimeCtrl = function ($scope) {
     $scope.open = function($event) {
       $event.preventDefault();
@@ -62,6 +94,7 @@ app.controller("SearchCtrl", function($scope, $http) {
     };
   };
 
+  // Detect Max Date
   DetectMaxDateCtrl = function ($scope) {
     $scope.today = function() {
       $scope.detect_max = new Date();
@@ -86,24 +119,119 @@ app.controller("SearchCtrl", function($scope, $http) {
     $scope.format = 'MMMM dd, yyyy';
   };
 
+  // Import Min Time
+  ImportMinTimeCtrl = function ($scope) {
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
 
+      $scope.opened = true;
+    };
+  };
 
+  // Import Min Date
+  ImportMinDateCtrl = function ($scope) {
+    $scope.today = function() {
+      $scope.import_min = new Date();
+    };
 
+    $scope.clear = function () {
+      $scope.import_min = null;
+    };
 
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
 
-  // ngShow boolean
-  $scope.searched = false;
+      $scope.opened = true;
+    };
 
-  // Sort initial type
-  $scope.orderBy = 'import_time';
-  $scope.order = 'desc';
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
 
-  // Pagination Settings
-  $scope.itemsPerPage = 20;
-  $scope.maxSize = 10;
-  $scope.currentPage = 1;
+    $scope.format = 'MMMM dd, yyyy';
+  };
 
-  // Search page top form requests
+  // Import Max Time
+  ImportMaxTimeCtrl = function ($scope) {
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+  };
+
+  // Import Max Date
+  ImportMaxDateCtrl = function ($scope) {
+    $scope.today = function() {
+      $scope.import_max = new Date();
+    };
+
+    $scope.clear = function () {
+      $scope.import_max = null;
+    };
+
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+
+    $scope.format = 'MMMM dd, yyyy';
+  };
+
+  // Filter detect date/time
+  $scope.filterDetect = function() {
+    $scope.detect_min.setUTCDate(m.filter_detect_min.getUTCDate());
+    $scope.detect_min.setUTCMonth(m.filter_detect_min.getUTCMonth());
+    $scope.detect_min.setUTCFullYear(m.filter_detect_min.getUTCFullYear());
+
+    $scope.detect_min.setUTCHours(m.filter_detect_min.getUTCHours());
+    $scope.detect_min.setUTCMinutes(m.filter_detect_min.getUTCMinutes());
+    $scope.detect_min.setUTCSeconds(m.filter_detect_min.getUTCSeconds());
+
+    $scope.detect_max.setUTCDate(m.filter_detect_max.getUTCDate());
+    $scope.detect_max.setUTCMonth(m.filter_detect_max.getUTCMonth());
+    $scope.detect_max.setUTCFullYear(m.filter_detect_max.getUTCFullYear());
+
+    $scope.detect_max.setUTCHours(m.filter_detect_max.getUTCHours());
+    $scope.detect_max.setUTCMinutes(m.filter_detect_max.getUTCMinutes());
+    $scope.detect_max.setUTCSeconds(m.filter_detect_max.getUTCSeconds());
+
+    $scope.update();
+  };
+
+  // Filter import date/time
+  $scope.filterImport = function() {
+    $scope.import_min.setUTCDate(m.filter_import_min.getUTCDate());
+    $scope.import_min.setUTCMonth(m.filter_import_min.getUTCMonth());
+    $scope.import_min.setUTCFullYear(m.filter_import_min.getUTCFullYear());
+
+    $scope.import_min.setUTCHours(m.filter_import_min.getUTCHours());
+    $scope.import_min.setUTCMinutes(m.filter_import_min.getUTCMinutes());
+    $scope.import_min.setUTCSeconds(m.filter_import_min.getUTCSeconds());
+
+    $scope.import_max.setUTCDate(m.filter_import_max.getUTCDate());
+    $scope.import_max.setUTCMonth(m.filter_import_max.getUTCMonth());
+    $scope.import_max.setUTCFullYear(m.filter_import_max.getUTCFullYear());
+
+    $scope.import_max.setUTCHours(m.filter_import_max.getUTCHours());
+    $scope.import_max.setUTCMinutes(m.filter_import_max.getUTCMinutes());
+    $scope.import_max.setUTCSeconds(m.filter_import_max.getUTCSeconds());
+
+    $scope.update();
+  };
+
+  // Search form
   $scope.search = function(artifact) {
     $scope.term = artifact.term;
     $scope.type = artifact.type;
@@ -113,7 +241,7 @@ app.controller("SearchCtrl", function($scope, $http) {
     $scope.update();
   };
 
-  // Update artifacts for search
+  // Search artifacts update functions
   $scope.updateFqdn = function(fqdn) {
     $scope.type = 'fqdn';
     $scope.term = fqdn;
@@ -127,6 +255,10 @@ app.controller("SearchCtrl", function($scope, $http) {
     document.getElementById("search-type").value = 'ipv4';
     document.getElementById("search-term").value = ipv4;
   }
+
+  $scope.$watch ('term', function () {
+    $scope.update();
+  });
 
   // Sort button functions
   $scope.sortDetect = function() {
@@ -163,7 +295,7 @@ app.controller("SearchCtrl", function($scope, $http) {
 
   };
 
-  // Pagination Settings
+  // Pagination functions
   $scope.setPage = function (pageNo) {
     $scope.currentPage = pageNo;
   };
@@ -171,10 +303,6 @@ app.controller("SearchCtrl", function($scope, $http) {
   $scope.pageChanged = function() {
     console.log('Page changed to: ' + $scope.currentPage);
   };
-
-  // Filter Button Collapse Initial state
-  $scope.isCollapsedImport = true;
-  $scope.isCollapsedDetect = true;
 
   $scope.$watch ('currentPage', function () {
     $scope.update();
@@ -189,13 +317,7 @@ app.controller("SearchCtrl", function($scope, $http) {
     $scope.update();
   });
 
-
-  $scope.$watch ('term', function () {
-    $scope.update();
-  });
-
-
-  // API requests Function
+  // API request function
   $scope.update = function () {
 
     if ($scope.type === 'ipv4') {
@@ -206,12 +328,16 @@ app.controller("SearchCtrl", function($scope, $http) {
             order_by: $scope.orderBy,
             order: $scope.order,
             timing: 1,
+            import_time_min: $scope.import_min,
+            import_time_max: $scope.import_max,
+            detect_time_min: $scope.detect_min,
+            detect_time_max: $scope.detect_max,
             ipv4: $scope.term
           }).
           success(function (data) {
             $scope.query = data;
 
-            // For pagination
+            // Pagination total items
             $scope.totalItems = parseInt($scope.query.total_events);
           }).
           then(function() {
@@ -226,12 +352,16 @@ app.controller("SearchCtrl", function($scope, $http) {
             order_by: $scope.orderBy,
             order: $scope.order,
             timing: 1,
+            import_time_min: $scope.import_min,
+            import_time_max: $scope.import_max,
+            detect_time_min: $scope.detect_min,
+            detect_time_max: $scope.detect_max,
             fqdn: $scope.term
           }).
           success(function (data) {
             $scope.query = data;
 
-            // For pagination
+            // Pagination total items
             $scope.totalItems = parseInt($scope.query.total_events);
           }).
           then(function() {
@@ -243,81 +373,4 @@ app.controller("SearchCtrl", function($scope, $http) {
 
 
 
-}); // End searchCtrl
-
-
-var DatepickerMinCtrl = function ($scope) {
-  $scope.today = function() {
-    $scope.date = new Date();
-    $scope.date.setDate($scope.date.getDate() -30);
-  };
-  $scope.today();
-
-  $scope.clear = function () {
-    $scope.date = null;
-  };
-
-  $scope.open = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-
-    $scope.opened = true;
-  };
-
-  $scope.dateOptions = {
-    formatYear: 'yy',
-    startingDay: 1
-  };
-
-  $scope.format = 'MMMM dd, yyyy';
-};
-
-
-var TimepickerMinCtrl = function ($scope) {
-  $scope.date = new Date();
-
-  $scope.open = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-
-    $scope.opened = true;
-  };
-};
-
-
-var DatepickerMaxCtrl = function ($scope) {
-  $scope.today = function() {
-    $scope.date = new Date();
-  };
-  $scope.today();
-
-  $scope.clear = function () {
-    $scope.date = null;
-  };
-
-  $scope.open = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-
-    $scope.opened = true;
-  };
-
-  $scope.dateOptions = {
-    formatYear: 'yy',
-    startingDay: 1
-  };
-
-  $scope.format = 'MMMM dd, yyyy';
-};
-
-
-var TimepickerMaxCtrl = function ($scope) {
-  $scope.date = new Date();
-
-  $scope.open = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-
-    $scope.opened = true;
-  };
-};
+}); // End MainCtrl
