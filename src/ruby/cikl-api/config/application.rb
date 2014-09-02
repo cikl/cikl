6da87217d580edd3bd1b370cb510d9d55b8d4dec
justfalter <falter@gmail.com>
2014-09-02 15:28:03 -0500

    Remove Bundler.require, causes problems
    -  Bundle.require requires all gems in the specified Bundler group. This
       is unneeded for our project, and actually causes issues if
       :development is specified.

--- src/ruby/cikl-api/config/application.rb
+++ src/ruby/cikl-api/config/application.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'app
 $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib')))
 
 require 'boot'
-Bundler.require :default, ENV['RACK_ENV']
+Bundler.require :default
 require 'cikl/config_loader'
 module Cikl
   Config = Cikl::ConfigLoader.load(File.expand_path('../config.yaml', __FILE__), ENV['RACK_ENV'])
