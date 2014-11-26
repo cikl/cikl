7f9d9837d12035b968fdb090765d941908bd7b41
justfalter <falter@gmail.com>
2014-11-26 13:01:39 -0600

    Go back to using build matrix for Travis

--- travis/ruby/before_install.sh
+++ travis/ruby/before_install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -x
+set -e
 source $TRAVIS_BUILD_DIR/travis/error_handler.sh
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.4.deb 
+sudo dpkg -i --force-confnew elasticsearch-1.2.4.deb
+sudo service elasticsearch restart
 sudo apt-get install libunbound2 unbound libxml2-dev
